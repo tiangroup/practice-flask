85ea43121cf160671d212582db76ad8b1839155c
Игорь Павленко <pavlenko@it-tian.ru>
2025-04-14 11:31:54 +0500

    test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     test_var = os.getenv('TEST_VAR', 'Переменная не задана')
-    return f'<h2>{test_var}</h2>'
+    return f'<h2>-{test_var}-</h2>'
 
 
 if __name__ == '__main__':
